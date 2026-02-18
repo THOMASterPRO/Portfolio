464f8d067dda8ce5f0f466338904f745bf0e4156
THOMASterPRO <thomas@bezemer.net>
2026-02-18 19:37:52 +0100

    ghpages fix

--- portfolio-app/src/main.jsx
+++ portfolio-app/src/main.jsx
@@ -1,10 +1,13 @@
 import { StrictMode } from 'react'
 import ReactDOM from "react-dom/client";
+import { HashRouter } from "react-router-dom";
 import './index.css'
 import App from './App.jsx'
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <StrictMode>
+  <HashRouter>
+    <StrictMode>
       <App />
-  </StrictMode>,
+    </StrictMode>
+  </HashRouter>
 )
